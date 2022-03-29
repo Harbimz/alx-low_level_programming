32b6fb00917c0aeaff1d39d1edc8c39be9fd0194
Harbimz <63602412+Harbimz@users.noreply.github.com>
2022-03-29 13:57:11 +0100

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,30 +1,33 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * *_strstr - description
- * @haystack: string
- * @needle: pointer
- * Return: pointer
+ * _strpbrk - Search a string for any of a set of bytes.
+ * @s: string
+ * @accept: string to match
+ * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
+ * or NULL if no such byte is found.
  */
 
-
-char *_strstr(char *haystack, char *needle)
+char *_strpbrk(char *s, char *accept)
 {
-int i, j;
+	int i, j;
+	char *p;
 
-for (i = 0; haystack[i] > '\0'; i++)
-{
-	for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		if (haystack[j] != needle[j - i])
+		j = 0;
+		while (accept[j] != '\0')
 		{
-			break;
+			if (accept[j] == s[i])
+			{
+				p = &s[i];
+				return (p);
+			}
+			j++;
 		}
+		i++;
 	}
-	if (needle[j - i] == '\0')
-	{
-		return (haystack + i);
-	}
-}
-return (0);
+
+	return (0);
 }
